cc1504609b13f0bc26611ee943f0baefb7a3d5f4
sushank04 <sushankchandel004@gmail.com>
2025-02-03 08:50:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import Services from "./componet/Services.jsx";
 import About from "./componet/About.jsx";
 import HappyClients from "./HappyClients.jsx";
 import WhyRegisterKaro from "./componet/WhyRegisterKaro.jsx";
-import AskQuestions from "./componet/AskQuestions.jsx";
+
 import Collaboration from "./componet/collaboration.jsx";
 import VideoIntro from "./componet/VideoIntro.jsx";
 import Testimonial from "./componet/Testimonial.jsx";
 import AppDownload from "./componet/AppDownload.jsx";
 import ContactUs from "./componet/ContactUs.jsx";
 import Footer from "./componet/Footer.jsx";
+import AskQuestions from "./componet/AskQuestions.jsx";
 
 const App = () => {
   return (
