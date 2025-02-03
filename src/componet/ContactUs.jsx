e6ea887cacc58faa3e032ee49afec3a996f1bc52
sushank04 <sushankchandel004@gmail.com>
2025-02-03 10:00:31 +0530

    m

--- src/componet/ContactUs.jsx
+++ src/componet/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
   return (
     <>
       <div className="contact-container">
-        <h2 className="heading-3">1% OF THE INDUSTRY</h2>
-        <h2 className="heading-4">Welcome to your new digital reality. Now.</h2>
+        <h2 className="heading">1% OF THE INDUSTRY</h2>
+        <h2 className="heading1">Welcome to your new digital reality. Now.</h2>
         <form>
           <input type="email" placeholder="Enter your Email" />
           <button type="submit">Submit</button>
